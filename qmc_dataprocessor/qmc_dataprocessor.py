674feda89cb83b166aec2a1b587d45aa6d379cf4
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2021-09-05 17:53:28 +0200

    Added basic setup for convertion to exe

---
+++ qmc_dataprocessor/qmc_dataprocessor.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+def GUI_window():
+    
+    GUI = Tk()
+
+    GUI.title("QMC Data Processor")
+    GUI.resizable(False, False)
+
+    GUI.mainloop()
+
+
+if __name__== "__main__":
+    GUI_window()
